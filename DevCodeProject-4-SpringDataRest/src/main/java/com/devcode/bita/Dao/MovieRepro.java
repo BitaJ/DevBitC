034304409abe5b016066b34b7a694fb340a5ba3a
BitaJ <BitaJ@192.168.0.147>
2018-07-29 15:06:21 +0200

    3rd commit, removed some unused imports from the Repro interfaces

--- DevCodeProject-4-SpringDataRest/src/main/java/com/devcode/bita/Dao/MovieRepro.java
+++ DevCodeProject-4-SpringDataRest/src/main/java/com/devcode/bita/Dao/MovieRepro.java
@@ -4,8 +4,6 @@ import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-import com.devcode.bita.model.Actor;
 import com.devcode.bita.model.Movie;
 
 	@Transactional
